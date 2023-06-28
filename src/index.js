5095c7da2a6a07c13862ec0a7a816c7bdde22602
Arthur Lopes <arthurlopr12@gmail.com>
2023-06-28 20:59:34 -0300

    userController

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const { log } = require('console');
 const http = require('http');
+const rooutes = require('./routes');
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-type': 'text/html' });
-    response.end('<h1>  Word!</h1>');
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url} `);
+
+    const route = routes.find((routeObj) => {
+        routeObj.endpoint === request.url && routeObj.method === require.method
+    });
+
+    if (route) {
+        route.handler(request, response);
+    } else {
+        response.writeHead(404, { 'Content-type': 'text/html' });
+        response.end(`Canot ${request.method} ${request.url}`);
+    }
 });
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
+
+
+
+    // if (request.url === '/users' && request.method === 'GET') {
+    //     UserController.listUsers(request, response)
+    // } else {
+
+    // }
